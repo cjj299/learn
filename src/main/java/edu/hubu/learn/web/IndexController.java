49f7dfe9462fd09cefb21c8527351575e197d6dc
Your Name <1809252506@qq.com>
2019-10-09 20:55:53 +0800

    cjj

--- src/main/java/edu/hubu/learn/web/IndexController.java
+++ src/main/java/edu/hubu/learn/web/IndexController.java
@@ -1,44 +1,45 @@
 package edu.hubu.learn.web;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.ModelAndView;
+    import org.springframework.stereotype.Controller;
+    import org.springframework.web.bind.annotation.RequestMapping;
+    import org.springframework.web.servlet.ModelAndView;
 
-import edu.hubu.learn.entity.User;
-import edu.hubu.learn.service.UserService;
-import edu.hubu.learn.entity.Blog;
-import edu.hubu.learn.service.BlogService;
+    import edu.hubu.learn.entity.User;
+    import edu.hubu.learn.service.UserService;
+    import edu.hubu.learn.entity.Blog;
+    import edu.hubu.learn.service.BlogService;
 
-@Controller
-@RequestMapping("/")
-public class IndexController {
-
-    @Autowired
-    private UserService userService;
-    @Autowired
-    private BlogService blogService;
+    @Controller
     @RequestMapping("/")
-    public ModelAndView index() {
-        ModelAndView mav = new ModelAndView();
-        mav.setViewName("index");
-        return mav;
-    }
+    public class IndexController {
 
-    @RequestMapping("/user")
-    public ModelAndView user() {
-        ModelAndView mav = new ModelAndView();
-        User user = userService.getUser(1l);
-        mav.addObject("user", user);
-        mav.setViewName("user");
-        return mav;
+        @Autowired
+        private UserService userService;
+        @Autowired
+        private BlogService blogService;
+        @RequestMapping("/")
+        public ModelAndView index() {
+            ModelAndView mav = new ModelAndView();
+            mav.setViewName("index");
+            return mav;
+        }
+
+       
+        @RequestMapping("/blog")
+        public ModelAndView blog() {
+            ModelAndView mav = new ModelAndView();
+            Blog blog = blogService.getBlog(1l);
+            mav.addObject("blog", blog);
+            mav.setViewName("blog");
+            return mav;
+        }
+        @RequestMapping("/user")
+        public ModelAndView user() {
+            ModelAndView mav = new ModelAndView();
+           User user= userService.getUser(1l);
+            mav.addObject("user",user);
+            mav.setViewName("user");
+            return mav;
+        }
     }
-    @RequestMapping("/blog")
-    public ModelAndView blog() {
-        ModelAndView mav = new ModelAndView();
-        Blog blog = blogService.getBlog(1l);
-        mav.addObject("blog", blog);
-        mav.setViewName("blog");
-        return mav;
-    }
-}
---
+++ src/main/java/edu/hubu/learn/web/UserController.java
@@ -0,0 +1,43 @@
+package edu.hubu.learn.web;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+    import org.springframework.web.servlet.ModelAndView;
+
+    import edu.hubu.learn.entity.User;
+    import edu.hubu.learn.service.UserService;
+    import edu.hubu.learn.entity.Blog;
+    import edu.hubu.learn.service.BlogService;
+
+    @Controller
+    @RequestMapping("/user")
+    public class UserController {
+
+        @Autowired
+        private UserService userService;
+       
+
+        @RequestMapping("/{id}")
+        public ModelAndView user(@PathVariable Long id)
+         {
+            ModelAndView mav = new ModelAndView();
+            User user = userService.getUser(id);
+            mav.addObject("user", user);
+            mav.setViewName("user");
+            return mav;
+        }
+        
+        
+        @RequestMapping("/list")
+        public ModelAndView uers() {
+            ModelAndView mav = new ModelAndView();
+           List<User> users = userService.getUsers();
+            mav.addObject("users", users);
+            mav.setViewName("users");
+            return mav;
+        }
+    }
